40b5fdee1f16b3d78b44392e5efbca0a1230d630
Samarajya Shrestha <samarajya@dzangolab.com>
2021-11-06 16:21:51 +0545

    feat: React Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [monsters, setMonsters] = useState([
+    {
+      id: 1,
+      name: "Goblin",
+    },
+    {
+      id: 2,
+      name: "Orc",
+    },
+    {
+      id: 3,
+      name: "Troll",
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {monsters.map((monster) => (
+        <h2 key={monster.id}>{monster.name}</h2>
+      ))}
     </div>
   );
 }
